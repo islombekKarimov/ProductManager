ff3e9bd12f5a87d2948b82b3d6190ccd1279ab78
islombekKarimov <karimovislombek1994@gmail.com>
2020-04-16 21:35:11 +0300

    Added JUnit test

--- src/main/java/com/product/controller/ProductController.java
+++ src/main/java/com/product/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -35,7 +32,7 @@ public class ProductController {
      return "new_product";
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping (value = "/save")
     public String saveProduct(@ModelAttribute("product") Product product){
        productService.saveProduct(product);
         return "redirect:/";
