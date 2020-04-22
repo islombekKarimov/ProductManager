d8149f898f56c7c97ab22b380a5f98c569b29633
islombekKarimov <karimovislombek1994@gmail.com>
2020-04-22 23:47:00 +0300

    Added UserRepository

--- src/main/java/com/product/entity/User.java
+++ src/main/java/com/product/entity/User.java
@@ -23,9 +23,5 @@ public class User {
         this.id = id;
     }
 
-    public User(String name, int id) {
 
-        this.name = name;
-        this.id = id;
-    }
 }
---
+++ src/main/java/com/product/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.product.repository;
+
+/**
+ * Created by Islombek Karimov on 22.04.2020.
+ */
+public interface UserRepository {
+}
