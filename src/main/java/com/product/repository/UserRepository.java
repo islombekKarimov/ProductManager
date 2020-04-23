3f6df201d7d95b5147c368422ef2b19a569a0c0e
islombekKarimov <karimovislombek1994@gmail.com>
2020-04-23 22:05:47 +0300

    Added UserRepository

--- src/main/java/com/product/repository/UserRepository.java
+++ src/main/java/com/product/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.product.repository;
 
+
+import com.product.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * Created by Islombek Karimov on 22.04.2020.
  */
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User,Integer> {
 }
