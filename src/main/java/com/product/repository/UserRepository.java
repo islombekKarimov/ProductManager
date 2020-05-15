f925a927b139bd36e0bdd5ea7ab9ebf5d6a4516a
islombekKarimov <karimovislombek1994@gmail.com>
2020-05-15 22:31:44 +0300

    Test

--- src/main/java/com/product/repository/UserRepository.java
+++ src/main/java/com/product/repository/UserRepository.java
@@ -3,9 +3,10 @@ package com.product.repository;
 
 import com.product.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * Created by Islombek Karimov on 22.04.2020.
  */
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends PagingAndSortingRepository<User,Integer> {
 }
