e3d6e64c192c72cfbf31355f049df2cf82a65d77
islombekKarimov <karimovislombek1994@gmail.com>
2020-05-16 22:46:12 +0300

    Updated User class

--- src/main/java/com/product/entity/User.java
+++ src/main/java/com/product/entity/User.java
@@ -1,10 +1,22 @@
 package com.product.entity;
 
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by Islombek Karimov on 16.04.2020.
  */
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false, insertable = false)
     private int id;
+
+    @Column(name = "name", nullable = false)
+    @NotNull(message = "user name can't be null")
     private String name;
 
     public String getName() {
