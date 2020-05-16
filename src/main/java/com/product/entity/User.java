92d1ee0ad299475114fe277d06a6a3f644b70fac
islombekKarimov <karimovislombek1994@gmail.com>
2020-05-16 23:19:28 +0300

    Updated User class

--- src/main/java/com/product/entity/User.java
+++ src/main/java/com/product/entity/User.java
@@ -1,6 +1,8 @@
 package com.product.entity;
 
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
@@ -18,6 +20,31 @@ public class User {
     @Column(name = "name", nullable = false)
     @NotNull(message = "user name can't be null")
     private String name;
+
+    @Column(name = "login", nullable = false)
+    @NotNull(message = "User login can't be null")
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    @NotNull(message = "User password can't be null")
+    @Length(min = 3)
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
 
     public String getName() {
         return name;
