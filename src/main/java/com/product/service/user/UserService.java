52943a68a136ad5ce7c2c22aa8ffd674f9e0a71b
islombekKarimov <karimovislombek1994@gmail.com>
2020-05-17 23:00:54 +0300

    Updated UserService

---
+++ src/main/java/com/product/service/user/UserService.java
@@ -0,0 +1,22 @@
+package com.product.service.user;
+
+import com.product.entity.User;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.stereotype.Service;
+import sun.corba.EncapsInputStreamFactory;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+/**
+ * Created by Islombek Karimov on 25.04.2020.
+ */
+@Service
+public interface UserService {
+    User create(User user) throws ConstraintViolationException, EntityNotFoundException;
+    User findById(int id) throws EntityNotFoundException;
+    User update(User user, int id) throws EntityNotFoundException;
+    void delete(int id) throws EntityNotFoundException;
+    List<User> findAll();
+
+}
