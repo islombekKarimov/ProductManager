3ca51b3d505a4df44e6557190b747dd9039944ba
islombekKarimov <karimovislombek1994@gmail.com>
2020-05-21 22:01:59 +0300

    updated UserController

--- src/main/java/com/product/controller/UserController.java
+++ src/main/java/com/product/controller/UserController.java
@@ -6,8 +6,9 @@ import com.product.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by Islombek Karimov on 16.05.2020.
@@ -22,13 +23,28 @@ public class UserController {
     ProductService productService;
 
 
-    @RequestMapping(value = {"registration_page"}, method = RequestMethod.GET)
+    @GetMapping(value = {"/registration_page"})
     public String registrationPage(Model model) {
         User user = new User();
         model.addAttribute("user", user);
         return "registration";
     }
 
+    @PostMapping(value = {"/save_user"})
+    public String saveUser(@ModelAttribute(name = "user") User user ){
+        System.out.println(user.getName());
+        System.out.println(user.getLogin());
+        System.out.println(user.getPassword());
+        userService.create(user);
+    return "redirect:/user_list";
+    }
+
+    @RequestMapping(value = {"/user_list"})
+    public String userList(Model model){
+        List<User> userList = userService.findAll();
+        model.addAttribute("userList", userList);
+        return "user.list";
+    }
 
 
 
