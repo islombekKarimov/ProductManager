059f3ea5bf9fd6a01bb6014657a0ae3297d249d9
islombekKarimov <karimovislombek1994@gmail.com>
2020-05-22 22:56:21 +0300

    updated UserController

--- src/main/java/com/product/repository/UserRepository.java
+++ src/main/java/com/product/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.product.repository;
 
 
 import com.product.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
 
 /**
  * Created by Islombek Karimov on 22.04.2020.
  */
-public interface UserRepository extends JpaRepository<User,Integer> {
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<User,Integer> {
 }
