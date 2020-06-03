0ac532132a003601b2aeadbd6c0740fad838091b
islombekKarimov <karimovislombek1994@gmail.com>
2020-06-03 22:21:45 +0300

    updated LoginController

--- src/main/java/com/product/controller/LoginController.java
+++ src/main/java/com/product/controller/LoginController.java
@@ -1,10 +1,16 @@
 package com.product.controller;
 
+import com.product.entity.User;
 import com.product.service.product.ProductService;
 import com.product.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by Islombek Karimov on 08.05.2020.
@@ -19,10 +25,33 @@ public class LoginController {
     ProductService productService;
 
     @GetMapping(value = {"/", "/login"})
-    public String loginPage() {
+    public String loginPage(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
         return "login";
     }
 
+
+    @PostMapping(value = {"/user_login"})
+    public ModelAndView checkUserByLogin(@ModelAttribute(name = "user") User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        List<User> userList = userService.getUserByLogin(user.getLogin());
+        if (userList.size() == 0) {
+            bindingResult.rejectValue("login", "error.login", "You don't register");
+            modelAndView.setViewName("login");
+        }
+        if (userList.size() > 0) {
+            for (User listUser : userList) {
+                if (listUser.getLogin().equals(user.getLogin()) && listUser.getPassword().equals(user.getPassword())) {
+                    modelAndView.addObject("redirect:/user_list");
+                } else {
+                    bindingResult.rejectValue("password", "error.password", "Your Login or Password incorrect");
+                    modelAndView.setViewName("login");
+                }
+            }
+        }
+        return modelAndView;
+    }
 
 
 }
