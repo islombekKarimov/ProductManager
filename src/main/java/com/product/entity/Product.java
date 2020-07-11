cb4c0024a15309627c99a607803669c66aa8151f
islombekKarimov <karimovislombek1994@gmail.com>
2020-07-11 23:51:27 +0500

    updated Product

--- src/main/java/com/product/entity/Product.java
+++ src/main/java/com/product/entity/Product.java
@@ -3,90 +3,30 @@ package com.product.entity;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
-/**
- * Created by Islombek Karimov on 21.03.2020.
- */
-
+/** Created by Islombek Karimov on 21.03.2020. */
 @Entity
-@Table(name = "Product")
-public class Product {
+@Table(name = "product")
+public class Product extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, insertable = false, updatable = false)
-    private int id ;
-    @Column(name = "name", nullable = false)
-    @NotNull(message = "product name con't be null")
-    private String name;
-    @Column(name = "brand", nullable = false)
-    @NotNull(message = "product brand can't be null")
-    private String brand;
-    @Column(name = "made_in", nullable = false)
-    @NotNull(message = "product made in can't be null")
-    private String madeIn;
-    @Column(name = "price", nullable = false)
-    @NotNull(message = "product price can't be null")
-    private double price;
-    @Column(name = "user_id", nullable = false)
-    @NotNull(message = "userId can't be null")
-    private int userId;
+  private static final long serialVersionUID = -6291879670469397777L;
 
-    public int getUserId() {
-        return userId;
-    }
+  @Column(name = "name", nullable = false)
+  @NotNull(message = "product name con't be null")
+  private String name;
 
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
+  @Column(name = "brand", nullable = false)
+  @NotNull(message = "product brand can't be null")
+  private String brand;
 
-    public int getId() {
-        return id;
-    }
+  @Column(name = "made_in", nullable = false)
+  @NotNull(message = "product made in can't be null")
+  private String madeIn;
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  @Column(name = "price", nullable = false)
+  @NotNull(message = "product price can't be null")
+  private double price;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getMadeIn() {
-        return madeIn;
-    }
-
-    public void setMadeIn(String madeIn) {
-        this.madeIn = madeIn;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", brand='" + brand + '\'' +
-                ", madeIn='" + madeIn + '\'' +
-                ", price=" + price +
-                '}';
-    }
+  @Column(name = "user_id", nullable = false)
+  @NotNull(message = "userId can't be null")
+  private int userId;
 }
