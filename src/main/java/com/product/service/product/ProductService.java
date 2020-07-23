e3d5997b1c7aa66d6eae5ea351cf6cc24ee1fb91
islombekKarimov <karimovislombek1994@gmail.com>
2020-07-23 22:48:30 +0500

    updated ProductService

--- src/main/java/com/product/service/product/ProductService.java
+++ src/main/java/com/product/service/product/ProductService.java
@@ -4,19 +4,18 @@ import com.product.dto.ProductDTO;
 import com.product.entity.Product;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 /** Created by Islombek Karimov on 21.03.2020. */
 @Service
 public interface ProductService {
-  ProductDTO create(ProductDTO dto) throws EntityNotFoundException;
+  ProductDTO create(ProductDTO dto) ;
 
-  ProductDTO get(Integer id) throws EntityNotFoundException;
+  ProductDTO get(Long id);
 
-  ProductDTO update(ProductDTO dto) throws EntityNotFoundException;
+  ProductDTO update(ProductDTO dto);
 
-  void delete(int id) throws EntityNotFoundException;
+  void delete(Long id);
 
-  List<Product> getProductListByUserId(int userId);
+  List<Product> getProductListByUserId(Long userId);
 }
