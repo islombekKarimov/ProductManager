cad5635c3e79c93fe783fe28eeb949a1ed63f080
islombekKarimov <karimovislombek1994@gmail.com>
2020-07-25 23:37:07 +0500

    added ApiPath

---
+++ src/main/java/com/product/constant/ApiPath.java
@@ -0,0 +1,12 @@
+package com.product.constant;
+
+/**
+ * ApiPath.
+ *
+ * @author Islombek Karimov
+ * @since 25.07.2020
+ */
+public interface ApiPath {
+  String API_USER = "/api/user";
+  String API_PRODUCT = "/api/product";
+}
