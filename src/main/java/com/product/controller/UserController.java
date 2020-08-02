dfff90ce7c043703a24f25eefd5b98379d0835fc
islombekKarimov <karimovislombek1994@gmail.com>
2020-08-02 22:26:25 +0300

    updated LoginController and ProductController

--- src/main/java/com/product/controller/UserController.java
+++ src/main/java/com/product/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.product.controller;
 
-import static com.product.constant.ApiPath.API_USER;
+//import static com.product.constant.ApiPath.API_USER;
+
+import com.product.dto.UserDTO;
 import com.product.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
 
 /** Created by Islombek Karimov on 16.05.2020. */
 @RestController
-@RequestMapping(value = API_USER)
+@RequestMapping(value ="/api/user")
 public class UserController {
 
   private UserService userService;
@@ -17,5 +23,10 @@ public class UserController {
     this.userService = userService;
   }
 
+  @PostMapping("/create")
+  public ResponseEntity create(@RequestBody  UserDTO userDTO) {
+     userService.create(userDTO);
+    return ResponseEntity.ok(HttpStatus.OK);
+  }
 
 }
