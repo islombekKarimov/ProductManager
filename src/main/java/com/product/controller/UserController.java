fe8536fd653f3b541fe76557bd5f1ee5b9f723c7
@karimovislombek1994 <karimovislombek1994@gmail.com>
2020-08-16 23:40:07 +0500

    updated UserController

--- src/main/java/com/product/controller/UserController.java
+++ src/main/java/com/product/controller/UserController.java
@@ -10,23 +10,47 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 
-/** Created by Islombek Karimov on 16.05.2020. */
+
+/**
+ * Created by Islombek Karimov on 16.05.2020.
+ */
+
 @RestController
-@RequestMapping(value ="/api/user")
+@RequestMapping(value = "/api/user")
 public class UserController {
 
-  private UserService userService;
+    private UserService userService;
 
-  @Autowired
-  public void setUserService(UserService userService) {
-    this.userService = userService;
-  }
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
-  @PostMapping("/create")
-  public ResponseEntity create(@RequestBody  UserDTO userDTO) {
-     userService.create(userDTO);
-    return ResponseEntity.ok(HttpStatus.OK);
-  }
+    @PostMapping("/create")
+    public ResponseEntity create(@RequestBody UserDTO userDTO) {
+        userService.create(userDTO);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
 
+    @PostMapping("/update")
+    public ResponseEntity update(@RequestBody UserDTO userDTO) {
+        userService.update(userDTO);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity delete(@PathVariable(name = "userId") Long id) {
+        userService.delete(id);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PostMapping("/get")
+    public ResponseEntity get(@Valid @PathVariable(name = "id") Long id) {
+        System.out.println("================= " + id);
+        userService.get(id);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
 }
+
