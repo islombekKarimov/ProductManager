440cb9d78872dccd028ad5964dadb92a9c7d1128
@karimovislombek1994 <karimovislombek1994@gmail.com>
2020-08-23 19:09:14 +0500

    updated Product

--- src/main/java/com/product/service/product/ProductService.java
+++ src/main/java/com/product/service/product/ProductService.java
@@ -2,20 +2,20 @@ package com.product.service.product;
 
 import com.product.dto.ProductDTO;
 import com.product.entity.Product;
-import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
-/** Created by Islombek Karimov on 21.03.2020. */
-@Service
+/**
+ * Created by Islombek Karimov on 21.03.2020.
+ */
 public interface ProductService {
-  ProductDTO create(ProductDTO dto) ;
+    ProductDTO create(ProductDTO dto);
 
-  ProductDTO get(Long id);
+    ProductDTO get(Long id);
 
-  ProductDTO update(ProductDTO dto);
+    ProductDTO update(ProductDTO dto);
 
-  void delete(Long id);
+    void delete(Long id);
 
-  List<Product> getProductListByUserId(Long userId);
+    Page<Product> getProductListByUserId(Pageable pageable, Long userId);
 }
--- src/main/java/com/product/service/product/impl/ProductServiceImpl.java
+++ src/main/java/com/product/service/product/impl/ProductServiceImpl.java
@@ -1,55 +1,72 @@
 package com.product.service.product.impl;
 
+import com.product.converter.ProductConverter;
 import com.product.dto.ProductDTO;
 import com.product.entity.Product;
 import com.product.repository.ProductRepository;
 import com.product.service.product.ProductService;
-import com.product.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.List;
 import java.util.Optional;
 
-/** Created by Islombek Karimov on 08.05.2020. */
+/**
+ * Created by Islombek Karimov on 08.05.2020.
+ */
 @Service
 public class ProductServiceImpl implements ProductService {
-  private ProductRepository productRepository;
+    private ProductRepository productRepository;
 
-  @Autowired
-  public void setProductRepository(ProductRepository productRepository) {
-    this.productRepository = productRepository;
-  }
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
 
-  @Override
-  public ProductDTO create(ProductDTO dto) {
-    return null;
-  }
+    @Override
+    public ProductDTO create(ProductDTO dto) {
+        Product entity = ProductConverter.toEntity(dto);
+        return ProductConverter.toDTO(entity);
+    }
 
-  @Override
-  public ProductDTO get(Long id) {
-    return null;
-  }
+    @Override
+    public ProductDTO get(Long id) {
+        Optional<Product> optional = findById(id);
+        if (optional.isPresent()) {
+            return ProductConverter.toDTO(optional.get());
 
-  @Override
-  public ProductDTO update(ProductDTO dto)  {
-    return null;
-  }
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
 
-  @Override
-  public void delete(Long id) {
+    @Override
+    public ProductDTO update(ProductDTO dto) {
+        Optional<Product> optional = findById(dto.getId());
+        if (optional.isPresent()) {
+            Product entity = ProductConverter.toEntity(dto);
+            return ProductConverter.toDTO(productRepository.save(entity));
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
 
-  }
+    @Override
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
 
-  @Override
-  public List<Product> getProductListByUserId(Long userId) {
-    return null;
-  }
+    @Override
+    public Page<Product> getProductListByUserId(Pageable pageable, Long userId) {
+        return null;
+    }
+
+    private Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
 
 //  @Override
 //  public List<Product> getProductListByUserId(int userId) {
