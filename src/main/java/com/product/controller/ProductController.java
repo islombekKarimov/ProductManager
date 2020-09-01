15796ff895e68f19ff705f212db6a8e3b00f04c7
@karimovislombek1994 <karimovislombek1994@gmail.com>
2020-09-01 21:11:13 +0500

    update Product

--- src/main/java/com/product/controller/ProductController.java
+++ src/main/java/com/product/controller/ProductController.java
@@ -1,25 +1,44 @@
 package com.product.controller;
 
+import com.product.dto.ProductDTO;
 import com.product.service.product.ProductService;
 import com.product.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-/** Created by Islombek Karimov on 21.03.2020. */
+/**
+ * Created by Islombek Karimov on 21.03.2020.
+ */
 @RestController
 public class ProductController {
-  private ProductService productService;
-  private UserService userService;
+    private ProductService productService;
+    private UserService userService;
 
-  @Autowired
-  public ProductController setProductService(ProductService productService) {
-    this.productService = productService;
-    return this;
-  }
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
 
-  @Autowired
-  public ProductController setUserService(UserService userService) {
-    this.userService = userService;
-    return this;
-  }
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity create(@RequestBody ProductDTO dto) {
+        productService.create(dto);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity update(@RequestBody ProductDTO dto) {
+        productService.update(dto);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+
 }
