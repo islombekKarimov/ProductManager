aa0421553b6139ae1a49e7b64d9cb34f8905c585
Karimov Islombek Kadambayevich <IKAKarimov@beeline.uz>
2021-02-05 11:41:06 +0500

    added Spring Security

--- src/main/java/com/product/controller/UserController.java
+++ src/main/java/com/product/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.product.controller;
 
-//import static com.product.constant.ApiPath.API_USER;
+// import static com.product.constant.ApiPath.API_USER;
 
 import com.product.dto.UserDTO;
 import com.product.service.product.ProductService;
@@ -13,59 +13,60 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
-/**
- * Created by Islombek Karimov on 16.05.2020.
- */
-
+/** Created by Islombek Karimov on 16.05.2020. */
 @RestController
 @RequestMapping(value = "/api/user")
 public class UserController {
 
-    private UserService userService;
-    private ProductService productService;
+  private UserService userService;
+  private ProductService productService;
 
-    @Autowired
-    public void setUserService(UserService userService) {
-        this.userService = userService;
-    }
+  @Autowired
+  public void setUserService(UserService userService) {
+    this.userService = userService;
+  }
 
-    @Autowired
-    public void setProductService(ProductService productService) {
-        this.productService = productService;
-    }
+  @Autowired
+  public void setProductService(ProductService productService) {
+    this.productService = productService;
+  }
 
-    @PostMapping("/create")
-    public ResponseEntity<Object> create(@RequestBody UserDTO userDTO) {
-        userService.create(userDTO);
-        return ResponseEntity.ok(HttpStatus.OK);
-    }
+  @GetMapping("/test")
+  public ResponseEntity<Object> test() {
+    return ResponseEntity.ok(HttpStatus.OK);
+  }
 
-    @PostMapping("/update")
-    public ResponseEntity<Object> update(@RequestBody UserDTO userDTO) {
-        userService.update(userDTO);
-        return ResponseEntity.ok(HttpStatus.OK);
-    }
+  @PostMapping("/create")
+  public ResponseEntity<Object> create(@RequestBody UserDTO userDTO) {
+    userService.create(userDTO);
+    return ResponseEntity.ok(HttpStatus.OK);
+  }
 
-    @PostMapping("/delete/{id}")
-    public ResponseEntity<Object> delete(@PathVariable(name = "id") Long id) {
-        userService.delete(id);
-        return ResponseEntity.ok(HttpStatus.OK);
-    }
+  @PostMapping("/update")
+  public ResponseEntity<Object> update(@RequestBody UserDTO userDTO) {
+    userService.update(userDTO);
+    return ResponseEntity.ok(HttpStatus.OK);
+  }
 
-    @PostMapping("/get/{id}")
-    public ResponseEntity<Object> get(@PathVariable(name = "id") Long id) {
-        return ResponseEntity.ok(userService.get(id));
-    }
+  @PostMapping("/delete/{id}")
+  public ResponseEntity<Object> delete(@PathVariable(name = "id") Long id) {
+    userService.delete(id);
+    return ResponseEntity.ok(HttpStatus.OK);
+  }
 
-    @PostMapping("/get-list")
-    public Page<UserDTO> getList(@PageableDefault(size = 30, value = 0) Pageable pageable) throws Exception {
-            return userService.findAll(pageable);
-    }
+  @PostMapping("/get/{id}")
+  public ResponseEntity<Object> get(@PathVariable(name = "id") Long id) {
+    return ResponseEntity.ok(userService.get(id));
+  }
 
-    @PostMapping("/user-products/{id}")
-    public ResponseEntity<Object> getProductList(@PathVariable(name = "id") Long id) {
-        return  ResponseEntity.ok(productService.getProductByUserList(id));
-    }
+  @PostMapping("/get-list")
+  public Page<UserDTO> getList(@PageableDefault(size = 30, value = 0) Pageable pageable)
+      throws Exception {
+    return userService.findAll(pageable);
+  }
+
+  @PostMapping("/user-products/{id}")
+  public ResponseEntity<Object> getProductList(@PathVariable(name = "id") Long id) {
+    return ResponseEntity.ok(productService.getProductByUserList(id));
+  }
 }
-
--- src/main/java/com/product/security/SecurityConfig.java
+++ src/main/java/com/product/security/SecurityConfig.java
@@ -1,28 +1,49 @@
 package com.product.security;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedMethods("POST");
+  }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
-        http.httpBasic()
-                .disable()
-                .csrf()
-                .disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-                .and()
-                .exceptionHandling();
+  private RequestMappingHandlerMapping requestMappingHandlerMapping;
 
-    }
+  @Autowired
+  public void setRequestHandlerMapping(RequestMappingHandlerMapping requestHandlerMapping) {
+    this.requestMappingHandlerMapping = requestHandlerMapping;
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors();
+    http.httpBasic()
+        .disable()
+        .csrf()
+        .disable()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+        .and()
+        .exceptionHandling();
+  }
+
+  @Bean
+  PasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
