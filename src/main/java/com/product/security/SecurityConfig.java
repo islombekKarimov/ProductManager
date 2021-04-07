79fc767004291e03d02e6dea12302128f6c49ff7
Islombek Karimov <karimovislombek1994@gmail.com>
2021-04-07 17:06:36 +0500

    SpringSecurity added user permitions

--- src/main/java/com/product/security/SecurityConfig.java
+++ src/main/java/com/product/security/SecurityConfig.java
@@ -1,22 +1,49 @@
 package com.product.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
+
+    @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/swagger-ui.html/**/*", "/api/product/get-list")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .httpBasic();
+  }
+
+  @Override
+  @Bean
+  protected UserDetailsService userDetailsService() {
+    UserDetails user = User.builder()
+            .username("user")
+            .password(passwordEncoder.encode("admin"))
+            .roles("ADMIN")
+            .build();
+    return new InMemoryUserDetailsManager(user);
+  }
+
 }
