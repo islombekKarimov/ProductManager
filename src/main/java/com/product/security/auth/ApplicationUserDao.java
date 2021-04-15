fc2d220edbec2b50b3493d1a0e11f8e522fc2a8c
Islombek Karimov <karimovislombek1994@gmail.com>
2021-04-15 16:53:34 +0500

    bug fixed

--- src/main/java/com/product/security/auth/ApplicationUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.product.security.auth;
-
-import java.util.Optional;
-
-
-public interface ApplicationUserDao {
-
-    Optional<ApplicationUser> selectApplicationUserByUsername(String username);
-}
