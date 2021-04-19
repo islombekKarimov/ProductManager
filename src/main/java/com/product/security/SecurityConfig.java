d0d83b391963cdee677b731d5720714cce063eaa
Islombek Karimov <karimovislombek1994@gmail.com>
2021-04-19 17:03:51 +0500

    JWT added

--- src/main/java/com/product/security/SecurityConfig.java
+++ src/main/java/com/product/security/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.product.security;
 
 import com.product.enums.Role;
+import com.product.security.jwt.UserCredentials;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -27,9 +29,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-      http.authorizeRequests()
-//              .antMatchers("/swagger-ui.html/**/*")
-//              .permitAll()
+      http
+              .csrf().disable()
+              .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+              .and()
+              .addFilter(new UserCredentials(authenticationManager()))
+              .authorizeRequests()
+              .antMatchers("/swagger-ui.html/**/*")
+              .permitAll()
               .anyRequest()
               .authenticated()
               .and()
